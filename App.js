94321ea155f3da5dc7ad5ca2e3680e7b98d386df
KarlStevenRequinto <lerdrobott@gmail.com>
2022-12-28 06:48:37 +0800

    removing unused components

--- App.js
+++ App.js
@@ -1,11 +1,6 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import HeroContainer from "./src/components/hero-container";
-import HeroListHeader from "./src/components/hero-list-header";
-import DraftedContainer from "./src/components/drafted-container";
-import CenterLogo from "./src/components/center-logo";
-import SelectedPane from "./src/components/selected-pane";
-import LandingPage from "./src/main-screen/landing";
+import { StyleSheet, View } from 'react-native';
+
+import LandingPage from './src/main-screen/landing';
 export default function App() {
   return (
     <View style={styles.container}>
